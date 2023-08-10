d7b25b56ae47968737341b356478f2d09b611e2e
HungParfait <81432965+HungParfait@users.noreply.github.com>
2023-08-11 00:41:28 +0700

    Test

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   ignorePatterns: ['build/', 'dist/', 'node_modules/'],
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended'],
   plugins: ['react', 'prettier'],
   settings: {
     react: {
